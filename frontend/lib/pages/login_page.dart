4067f3782b61970a94f29a16a6c29239ccc0054e
maximevez <maxime.vezinet@gmail.com>
2025-11-12 10:01:44 +0100

    Update login_page.dart

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -1,9 +1,94 @@
 import 'package:flutter/material.dart';
 import '../widgets/app_bar.dart'; 
+import 'package:http/http.dart' as http; 
+import 'dart:convert'; 
+import '../main.dart';
 
-
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String _statusMessage = 'Veuillez vous connecter.';
+  bool _isLoading = false;
+
+  Future<void> _handleLogin() async {
+    setState(() {
+      _isLoading = true;
+      _statusMessage = 'Connexion en cours...';
+    });
+
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+        setState(() {
+          _statusMessage = 'Veuillez remplir tous les champs.';
+          _isLoading = false;
+        });
+        return;
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:5000/api/auth/login'), 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+      
+      // TODO: Stocker le token JWT (data['access_token']) localement
+
+        setState(() {
+          _statusMessage = data['connexion réussie']; 
+        });
+
+        Navigator.pushReplacement(
+            context,
+           MaterialPageRoute(builder: (context) => const TestPage()), 
+        );
+
+      } else if (response.statusCode == 401) {//erreur de connexion
+        final errorData = jsonDecode(response.body);
+        setState(() {
+          _statusMessage = errorData['message']; 
+        });
+      } else {
+        setState(() {
+          _statusMessage = 'Erreur serveur: ${response.statusCode}.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _statusMessage = 'Erreur de connexion. Vérifiez le serveur Flask.';
+      });
+   }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  // NETTOYAGE DES CONTRÔLEURS
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +110,8 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 40),
 
               // Champ de texte pour l'Email
-              const TextField(
+              TextField(
+                controller: _emailController,
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Email',
@@ -36,7 +122,8 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 20),
 
               // Champ de texte pour le Mot de Passe
-              const TextField(
+              TextField(
+                controller: _passwordController,
                 obscureText: true, // Cache le texte tapé
                 decoration: InputDecoration(
                   labelText: 'Mot de passe',
@@ -45,18 +132,20 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 30),
-
               // Bouton de Connexion
               ElevatedButton(
-                onPressed: () {
-                  // TODO: Ajouter ici la logique d'appel à l'API Flask /api/auth/login
-                  print('Tentative de connexion...');
-                },
+                onPressed: _isLoading ? null : _handleLogin, 
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.symmetric(vertical: 16),
                   textStyle: const TextStyle(fontSize: 18),
                 ),
-                child: const Text('Se Connecter'),
+                child: _isLoading 
+                    ? const SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: CircularProgressIndicator(color: Colors.white, strokeWidth: 3),
+                      ) 
+                    : const Text('Se Connecter'),
               ),
             ],
           ),
